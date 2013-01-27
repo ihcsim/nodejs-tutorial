e22625451c2da3021c957d282af7e8209381a406
Ivan Sim <ihcsim@gmail.com>
2013-01-26 22:37:51 -0800

    Sample codes for Node.js module.

---
+++ js/module.js
@@ -0,0 +1,4 @@
+var logger = require('./node-modules/logger.js');
+logger.log('A test log message');
+logger.warn('A test warning message');
+logger.error('A test error message');
---
+++ js/node-modules/logger.js
@@ -0,0 +1,12 @@
+exports.log = function(message){
+  console.log(message);
+};
+
+exports.warn = function(message){
+  console.warn(message);
+};
+
+exports.error = function(message){
+  console.error(message);
+};
+
