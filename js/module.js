ede7e72494720a521cf82f17414b910578da4d73
Ivan Sim <ihcsim@gmail.com>
2013-01-26 22:50:19 -0800

    Move the node-modules folder out of the js folder.

--- js/module.js
+++ js/module.js
@@ -1,4 +1,4 @@
-var logger = require('./node-modules/logger.js');
+var logger = require('../node-modules/logger.js');
 logger.log('A test log message');
 logger.warn('A test warning message');
 logger.error('A test error message');
