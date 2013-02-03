188eb00733a91aeea3bafd7e335c884c03514842
Ivan Sim <ihcsim@gmail.com>
2013-02-02 22:20:03 -0800

    Add watermark to textbox.

--- resources/chat-client/client.js
+++ resources/chat-client/client.js
@@ -3,7 +3,13 @@ $(document).ready(function(){
   var server = io.connect(CHAT_SERVER);  
   server.on('welcome', showGreetings);
 
+  addWaterMark();
+
   function showGreetings(msg){
     $("#title").text(msg['greetings']);
+  }
+
+  function addWaterMark(){
+    $("#outgoing-message").watermark("Enter your message here");
   }
 });
