f89af05ae8b8f7eca313c05566cff97664d0686f
Ivan Sim <ihcsim@gmail.com>
2013-02-02 22:11:05 -0800

    Extract show greetings codes to a separate function.

--- resources/chat-client/client.js
+++ resources/chat-client/client.js
@@ -1,8 +1,9 @@
 var CHAT_SERVER = "http://localhost:7000'"
 $(document).ready(function(){
-  var server = io.connect(CHAT_SERVER);
-  
-  server.on('welcome', function(msg){
+  var server = io.connect(CHAT_SERVER);  
+  server.on('welcome', showGreetings);
+
+  function showGreetings(msg){
     $("#title").text(msg['greetings']);
-  });
+  }
 });
