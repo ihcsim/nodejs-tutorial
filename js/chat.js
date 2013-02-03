cd96c6c998eb682939d066f73627f3149e38830a
Ivan Sim <ihcsim@gmail.com>
2013-02-02 22:01:14 -0800

    Update chat server to send greeting message on-connect.

--- js/chat.js
+++ js/chat.js
@@ -4,11 +4,9 @@
  */
 var express = require('express');
 var app = express();
-var http = require('http');
-var server = http.createServer(app).listen(7000);
+var server = require('http').createServer(app).listen(7000);
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function(client){
-  console.log('Client connected...');
-  //client.emit('message', {msg:'Welcome to chat room!'});
+  client.emit('welcome', {greetings:'Welcome to Chat Room!'});
 });
